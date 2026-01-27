5d936ae74fb7ca7251fc71920299ff2c913a58a7
Krzysztof Pawlowski <keniu201@gmail.com>
2026-01-27 17:10:31 +0100

    creating function calculateRentalCosts

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const cost = 40;
+  let result = 0;
+
+  if (days < 3) {
+    result = days * cost;
+  }
+
+  if (days >= 3 && days < 7) {
+    result = days * cost - 20;
+  }
+
+  if (days >= 7) {
+    result = days * cost - 50;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
